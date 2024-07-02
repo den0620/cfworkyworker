21350b5b849ed47dacdf2a4ab819ccca745c8c5d
ninefid <119758671+den0620@users.noreply.github.com>
2024-07-03 00:10:56 +0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -15,6 +15,20 @@ export default {
       if (response.headers.get("content-type") && response.headers.get("content-type").includes("text/html")) {
         let text = await response.text();
 
+        const imgRegex = /<img[^>]+src="([^">]+)"/g;
+        let match;
+
+        while ((match = imgRegex.exec(text)) !== null) {
+          const imgUrl = match[1];
+          const absoluteImgUrl = new URL(imgUrl, targetUrl).toString();
+          const imgResponse = await fetch(absoluteImgUrl);
+          const imgArrayBuffer = await imgResponse.arrayBuffer();
+          const base64String = arrayBufferToBase64(imgArrayBuffer);
+          const contentType = imgResponse.headers.get("content-type");
+          const dataUri = `data:${contentType};base64,${base64String}`;
+          text = text.replace(imgUrl, dataUri);
+        }
+
         text = text.replace(/<img/g, '<img loading="lazy"');
 
         return new Response(text, {
@@ -33,6 +47,16 @@ export default {
           Allow: "GET"
         }
       });
+    }
+
+    function arrayBufferToBase64(buffer) {
+      let binary = '';
+      const bytes = new Uint8Array(buffer);
+      const len = bytes.byteLength;
+      for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+      }
+      return btoa(binary);
     }
 
     if (request.method !== "GET") return methodNotAllowed(request);
